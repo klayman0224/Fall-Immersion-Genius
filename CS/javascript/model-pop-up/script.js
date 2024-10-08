c46e68dc1f23f32c8e7d5b43174bc86cd27b0be0
klayman0224 <ryan.fontenot@hgs.hiddengeniusproject.org>
2024-10-07 17:43:56 -0700

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,13 @@
-function modelPopUp() {
-    // Your code here.
-}   
+// Get modal element
+const modal = document.getElementById("myModal");
+// Get buttons
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const alertBtn = document.getElementById("alertBtn");
+//Open modal on button click
+openModalBtn.onclick = function() {
+}
+//Close modal on close button click
+closeModalBtn.onclick = function() {
+}
+//Close modal on outside click
