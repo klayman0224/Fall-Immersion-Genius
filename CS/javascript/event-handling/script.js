a4b7c2db5e12ba01182d07a74155d35c93c05713
klayman0224 <ryan.fontenot@hgs.hiddengeniusproject.org>
2024-10-07 17:48:25 -0700

    Update script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,5 @@
-function eventHandling() {
-    // Your code here.
-}
+document.getElementById('myButton').addEventListener('click', function() {
+    image = document.createElement('img');
+    image.src = 'https://www.google.com/imgres?q=skibidi%20toilet&imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2FWePNs-G7puA%2Fhq720.jpg%3Fsqp%3D-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGGUgXShFMA8%3D%26rs%3DAOn4CLC3dbKF2Jk8dzXTETyvDD2yhbgdeQ&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DWePNs-G7puA&docid=PiRd91Aezzv99M&tbnid=goC_QQfGTZ92mM&vet=12ahUKEwjTgci-_PCIAxUmle4BHZb9OWoQM3oECBsQAA..i&w=686&h=386&hcb=2&itg=1&ved=2ahUKEwjTgci-_PCIAxUmle4BHZb9OWoQM3oECBsQAA' ;
+    document.body.appendChild(image);
+});
