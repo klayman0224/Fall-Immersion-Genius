a614bdfbe8fc4e702a74f599f6b7cfcaea0fd8b8
klayman0224 <ryan.fontenot@hgs.hiddengeniusproject.org>
2024-11-08 17:55:55 -0800

    Update script.js

--- CS/javascript/modal pop up/script.js
+++ CS/javascript/modal pop up/script.js
@@ -1,13 +1,22 @@
 // Get modal element
-const modal = document.getElementById("myModal");
-// Get buttons
+const modal = document.getElementById("myModal"); 
+
 const openModalBtn = document.getElementById("openModalBtn");
-const closeModalBtn = document.getElementById("closeModalBtn");
-const alertBtn = document.getElementById("alertBtn");
-//Open modal on button click
-openModalBtn.onclick = function() {
-}
-//Close modal on close button click
-closeModalBtn.onclick = function() {
-}
-//Close modal on outside click
+const closeModalBtn = document.getElementById("closeModalBtn")
+const alertBtn = document.getElementById("alertBtn")
+
+// When the user clicks on the button, open the modal 
+openModalBtn.addEventListener("click", function() {
+    modal.style.display = "block"; // Show the modal
+    alert("This is an alert!"); // Show alert
+});
+// When the user clicks on <span> (x), close the modal
+closeModalBtn.addEventListener("click", function() {
+  modal.style.display = "none"; // Hide the modal
+});
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener("click", function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none"; // Close the modal if clicked outside
+    }
+});
